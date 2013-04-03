0d9d0fd4658b5a0593d0fca2e9f836bbf799ace6
Dan Milon <danmilon@gmail.com>
2013-04-03 17:29:14 +0300

    fix component support

--- src/component.js
+++ src/component.js
@@ -2,6 +2,7 @@ var d3 = require("../index");
 
 console.log(JSON.stringify({
   "name": "d3",
-  "version": d3.version,
-  "main": "./d3.js"
+  "version": "3.1.4",
+  "main": "index-browserify.js",
+  "scripts": ["index-browserify.js", "d3.js"]
 }, null, 2));
